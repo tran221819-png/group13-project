6c5509cb5ab7a8527d028214cde4267f3b02ac67
Ngọc Trân <tran221819@student.nctu.edu.vn>
2025-10-27 07:52:39 +0700

    Revert "feat: Integrate MongoDB Atlas and update controllers"
    This reverts commit 117d3a01b81fc15e8ab68b8137f50a9259e5cab2.

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,7 @@
-// File: backend/server.js
-require('dotenv').config(); // <--- THÊM DÒNG NÀY (Để đọc file .env)
 const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose'); // <--- THÊM DÒNG NÀY
-
-
 const app = express();
-app.use(cors());
 app.use(express.json());
-
-// --- KẾT NỐI MONGODB ---
-// Lấy chuỗi kết nối từ file .env
-const uri = process.env.MONGO_URI; 
-mongoose.connect(uri); // Bắt đầu kết nối
-
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully!");
-});
-// --- KẾT THÚC KẾT NỐI ---
-
-
-// Import user routes
 const userRoutes = require('./routes/user');
-app.use('/users', userRoutes);
-
-// PORT này giờ sẽ lấy từ file .env
-const PORT = process.env.PORT || 3000; 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.use('/api', userRoutes);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port ${PORT}"));
